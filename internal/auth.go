01a0ef396426a52c476dba8b873c60f97b19a3dd
leberKleber <max.marche@live.de>
2020-03-24 11:40:04 +0100

    impl pasword reset storage, provider and web

--- internal/auth.go
+++ internal/auth.go
@@ -9,8 +9,9 @@ import (
 	"time"
 )
 
-var ErrIncorrectPassword = fmt.Errorf("password incorrect")
-var ErrUserNotFound = fmt.Errorf("user not found")
+var ErrIncorrectPassword = errors.New("password incorrect")
+var ErrUserNotFound = errors.New("user not found")
+var ErrNoValidTokenFound = errors.New("no valid token found")
 var nowFunc = time.Now
 
 /**
@@ -18,7 +19,7 @@ Check email / password combination and return a new jwt if correct.
 return ErrIncorrectPassword when password is incorrect
 return UserNotFoundErr when user not found
 */
-func (p *Provider) Login(email, password string) (string, error) {
+func (p Provider) Login(email, password string) (string, error) {
 	u, err := p.Storage.User(email)
 	if err != nil {
 		if errors.Is(err, storage.ErrUserNotFound) {
@@ -39,7 +40,7 @@ func (p *Provider) Login(email, password string) (string, error) {
 CreatePasswordResetRequest send a password-reset-request email to the give address.
 return ErrUserNotFound when user does not exists
 */
-func (p *Provider) CreatePasswordResetRequest(email string) error {
+func (p Provider) CreatePasswordResetRequest(email string) error {
 	_, err := p.Storage.User(email)
 	if err != nil {
 		if errors.Is(err, storage.ErrUserNotFound) {
@@ -63,9 +64,55 @@ func (p *Provider) CreatePasswordResetRequest(email string) error {
 		return fmt.Errorf("failed to create password-reset-token for email %q: %w", email, err)
 	}
 
-	err = p.Mailer.SendPasswordResetRequestEMail(email, fmt.Sprintf(p.PasswordResetURLFmt, t))
+	err = p.Mailer.SendPasswordResetRequestEMail(email, fmt.Sprintf(p.PasswordResetURLFMT, t))
 	if err != nil {
 		return fmt.Errorf("failed to send password-reset-email: %w", err)
+	}
+
+	return nil
+}
+
+/**
+ResetPassword resets the password of the given account if the reset token is correct.
+*/
+func (p *Provider) ResetPassword(email, resetToken, newPassword string) error {
+	tokens, err := p.Storage.TokensByEMailAndToken(email, resetToken)
+	if err != nil {
+		return fmt.Errorf("faild to find all avalilable tokens: %w", err)
+	}
+
+	var t *storage.Token
+	for _, token := range tokens {
+		if token.Type == storage.TokenTypeReset {
+			//TODO check lifetime
+			t = &token
+			break
+		}
+	}
+
+	if t == nil {
+		return ErrNoValidTokenFound
+	}
+
+	u, err := p.Storage.User(email)
+	if err != nil {
+		return fmt.Errorf("failed to find user: %w", err)
+	}
+
+	securedPassword, err := bcryptPassword(newPassword)
+	if err != nil {
+		return fmt.Errorf("failed to bcrypt password: %w", err)
+	}
+	u.Password = securedPassword
+
+	err = p.Storage.UpdateUser(u)
+	if err != nil {
+		return fmt.Errorf("failed to update user: %w", err)
+	}
+
+	err = p.Storage.DeleteToken(t.ID)
+	if err != nil {
+		return fmt.Errorf("failed to delete token: %w", err)
 	}
 
 	return nil
